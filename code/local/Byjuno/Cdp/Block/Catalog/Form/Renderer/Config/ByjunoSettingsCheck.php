94f5e1d4e0c5d64cb493e3f6e86e286fa1870628
Igor Sutugin <igor.sutugin@ugcs.com>
2016-05-07 12:25:49 +0300

    .

--- code/local/Byjuno/Cdp/Block/Catalog/Form/Renderer/Config/ByjunoSettingsCheck.php
+++ code/local/Byjuno/Cdp/Block/Catalog/Form/Renderer/Config/ByjunoSettingsCheck.php
@@ -9,27 +9,42 @@ class Byjuno_Cdp_Block_Catalog_Form_Renderer_Config_ByjunoSettingsCheck extends 
         $paymentsEmailOrder = Mage::getStoreConfig(Mage_Sales_Model_Order::XML_PATH_EMAIL_COPY_TO, Mage::app()->getStore());
         $copyEmailOrder = '<span style="color: #FF0000">Not found</span>';
         $ok = 0;
-        if (stristr($paymentsEmailOrder, "info@byjuno.ch")) {
-            $copyEmailOrder = '<span style="color: #009600">OK</span>';
+        if (stristr($paymentsEmailOrder, "test-invoices@byjuno.ch")) {
+            $copyEmailOrder = '<span style="color: #D8D161">Test</span>';
             $ok++;
+        }
+        if (stristr($paymentsEmailOrder, "invoices@byjuno.ch") && !stristr($paymentsEmailOrder, "test")) {
+            $copyEmailOrder = '<span style="color: #009600">Production</span>';
+            $ok+=2;
         }
         //Mage_Sales_Model_Order_Invoice::XML_PATH_EMAIL_COPY_TO
         $paymentsEmailOrder = Mage::getStoreConfig(Mage_Sales_Model_Order_Invoice::XML_PATH_EMAIL_COPY_TO, Mage::app()->getStore());
         $copyEmailInvoice = '<span style="color: #FF0000">Not found</span>';
-        if (stristr($paymentsEmailOrder, "info@byjuno.ch")) {
-            $copyEmailInvoice = '<span style="color: #009600">OK</span>';
+        if (stristr($paymentsEmailOrder, "test-invoices@byjuno.ch")) {
+            $copyEmailInvoice = '<span style="color: #D8D161">Test</span>';
             $ok++;
+        }
+        if (stristr($paymentsEmailOrder, "invoices@byjuno.ch") && !stristr($paymentsEmailOrder, "test")) {
+            $copyEmailInvoice = '<span style="color: #009600">Production</span>';
+            $ok+=2;
         }
         //Mage_Sales_Model_Order_Shipment::XML_PATH_EMAIL_COPY_TO
         $paymentsEmailOrder = Mage::getStoreConfig(Mage_Sales_Model_Order_Shipment::XML_PATH_EMAIL_COPY_TO, Mage::app()->getStore());
         $copyEmailShipping = '<span style="color: #FF0000">Not found</span>';
-        if (stristr($paymentsEmailOrder, "info@byjuno.ch")) {
-            $copyEmailShipping = '<span style="color: #009600">OK</span>';
+        if (stristr($paymentsEmailOrder, "test-invoices@byjuno.ch")) {
+            $copyEmailShipping = '<span style="color: #D8D161">Test</span>';
             $ok++;
         }
-        $color = 'ddffdf';
-        if ($ok != 3) {
-            $color = 'FFE5E6';
+        if (stristr($paymentsEmailOrder, "invoices@byjuno.ch") && !stristr($paymentsEmailOrder, "test")) {
+            $copyEmailShipping = '<span style="color: #009600">Production</span>';
+            $ok+=2;
+        }
+        $color = 'FFE5E6';
+        if ($ok >= 3) {
+            $color = 'FFFEF2';
+        }
+        if ($ok == 6) {
+            $color = 'ddffdf';
         }
         return '<div style="white-space: nowrap;">
             <fieldset style="background-color: #'.$color.'">
