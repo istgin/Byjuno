bc19e1932109b4bcf8eaa6f382218a8dc1d56e06
Igor Sutugin <igor.sutugin@ugcs.com>
2016-05-22 13:35:01 +0300

    .

--- code/local/Byjuno/Cdp/Model/Standardinvoice.php
+++ code/local/Byjuno/Cdp/Model/Standardinvoice.php
@@ -18,6 +18,8 @@ class Byjuno_Cdp_Model_Standardinvoice extends Mage_Payment_Model_Method_Abstrac
     protected $_canRefund = true;
     protected $_canCapture = true;
     protected $_canCapturePartial = true;
+    protected $_canVoid = true;
+    protected $_canAuthorize = true;
 
     public function validate()
     {
@@ -33,10 +35,9 @@ class Byjuno_Cdp_Model_Standardinvoice extends Mage_Payment_Model_Method_Abstrac
 
     public function capture(Varien_Object $payment, $amount)
     {
-        $payment->setTransactionId(1);
-        $payment->setParentTransactionId($payment->getTransactionId());
-        $transaction = $payment->addTransaction(Mage_Sales_Model_Order_Payment_Transaction::TYPE_AUTH, null, true, "");
-        $transaction->setIsClosed(true);
+        $payment->setTransactionId($payment->getParentTransactionId().'-capture');
+        //$transaction = $payment->addTransaction(Mage_Sales_Model_Order_Payment_Transaction::TYPE_AUTH, null, true, "");
+        //$transaction->setIsClosed(true);
         return $this;
     }
 
@@ -70,7 +71,11 @@ class Byjuno_Cdp_Model_Standardinvoice extends Mage_Payment_Model_Method_Abstrac
             $status = $byjunoResponse->getProcessingInfoClassification();
             $this->getHelper()->saveS4Log($order, $request, $xml, $response, $status, $ByjunoRequestName);
         } else {
-            $this->getHelper()->saveS4Log($order, $request, $xml, "empty response", "0", $ByjunoRequestName);
+            $status = "ERR";
+            $this->getHelper()->saveS4Log($order, $request, $xml, "empty response", $status, $ByjunoRequestName);
+        }
+        if ($status == 'ERR') {
+            Mage::throwException(Mage::helper('payment')->__(Mage::getStoreConfig('payment/cdp/byjuno_s4_fail', Mage::app()->getStore())));
         }
         return $this;
     }
@@ -81,12 +86,6 @@ class Byjuno_Cdp_Model_Standardinvoice extends Mage_Payment_Model_Method_Abstrac
             return $this;
         }
         $order = $payment->getOrder();
-        if ($order->hasInvoices()) {
-            $invIncrementIDs = array();
-            foreach ($order->getInvoiceCollection() as $inv) {
-                $invIncrementIDs[] = $inv->getIncrementId();
-            }
-        }
         /* @var $request Byjuno_Cdp_Helper_Api_Classes_ByjunoS4Request */
         $request = $this->getHelper()->CreateMagentoShopRequestS5Paid($order, $requestedAmount);
         $ByjunoRequestName = 'Byjuno S5';
@@ -106,8 +105,55 @@ class Byjuno_Cdp_Model_Standardinvoice extends Mage_Payment_Model_Method_Abstrac
             $status = $byjunoResponse->getProcessingInfoClassification();
             $this->getHelper()->saveS4Log($order, $request, $xml, $response, $status, $ByjunoRequestName);
         } else {
-            $this->getHelper()->saveS4Log($order, $request, $xml, "empty response", "0", $ByjunoRequestName);
+            $status = 'ERR';
+            $this->getHelper()->saveS4Log($order, $request, $xml, "empty response", $status, $ByjunoRequestName);
         }
+        if ($status == 'ERR') {
+            Mage::throwException(Mage::helper('payment')->__(Mage::getStoreConfig('payment/cdp/byjuno_s5_fail', Mage::app()->getStore())));
+        }
+        /* @var $payent Mage_Sales_Model_Order_Payment */
+        $payment->setTransactionId($payment->getParentTransactionId().'-refund');
+        $transaction = $payment->addTransaction(Mage_Sales_Model_Order_Payment_Transaction::TYPE_VOID, null, true, "Transaction refunced");
+        $transaction->setIsClosed(true);
+        $transaction->save();
+        return $this;
+    }
+
+    public function cancel(Varien_Object $payment)
+    {
+        if (Mage::getStoreConfig('payment/cdp/byjunos5transacton', Mage::app()->getStore()) == '0') {
+            return $this;
+        }
+        /* @var $order Mage_Sales_Model_Order */
+        $order = $payment->getOrder();
+        /* @var $request Byjuno_Cdp_Helper_Api_Classes_ByjunoS4Request */
+        $request = $this->getHelper()->CreateMagentoShopRequestS5Paid($order, $order->getTotalDue());
+        $ByjunoRequestName = 'Byjuno S5';
+        $xml = $request->createRequest();
+        $byjunoCommunicator = new Byjuno_Cdp_Helper_Api_Classes_ByjunoCommunicator();
+        $mode = Mage::getStoreConfig('payment/cdp/currentmode', Mage::app()->getStore());
+        if ($mode == 'production') {
+            $byjunoCommunicator->setServer('live');
+        } else {
+            $byjunoCommunicator->setServer('test');
+        }
+        $response = $byjunoCommunicator->sendS4Request($xml, (int)Mage::getStoreConfig('payment/cdp/timeout', Mage::app()->getStore()));
+        $byjunoResponse = new Byjuno_Cdp_Helper_Api_Classes_ByjunoS4Response();
+        if ($response) {
+            $byjunoResponse->setRawResponse($response);
+            $byjunoResponse->processResponse();
+            $status = $byjunoResponse->getProcessingInfoClassification();
+            $this->getHelper()->saveS4Log($order, $request, $xml, $response, $status, $ByjunoRequestName);
+        } else {
+            $status = 'ERR';
+            $this->getHelper()->saveS4Log($order, $request, $xml, "empty response", $status, $ByjunoRequestName);
+        }
+        if ($status == 'ERR') {
+            Mage::throwException(Mage::helper('payment')->__(Mage::getStoreConfig('payment/cdp/byjuno_s5_fail', Mage::app()->getStore())));
+        }
+        $transaction = $payment->addTransaction(Mage_Sales_Model_Order_Payment_Transaction::TYPE_VOID, null, true, "Transaction canceled");
+        $transaction->setIsClosed(true);
+        $transaction->save();
         return $this;
     }
 
@@ -207,9 +253,21 @@ class Byjuno_Cdp_Model_Standardinvoice extends Mage_Payment_Model_Method_Abstrac
             if (intval($status) > 15) {
                 $status = 0;
             }
+            $trxId = $byjunoResponse->getResponseId();
         } else {
             $this->getHelper()->saveLog($quote, $request, $xml, "empty response", "0", $ByjunoRequestName);
+            $trxId = "empty";
         }
+        $payment->setTransactionId($trxId);
+        $payment->setParentTransactionId($payment->getTransactionId());
+        $transaction = $payment->addTransaction(Mage_Sales_Model_Order_Payment_Transaction::TYPE_AUTH, null, true, "");
+        if ($status == 2) {
+            $transaction->setIsClosed(false);
+        } else {
+            $transaction->setIsClosed(true);
+        }
+        $transaction->save();
+        $payment->save();
         $session->setData("intrum_status", $status);
         $session->setData("intrum_order", $order->getId());
         if ($status == 2) {
