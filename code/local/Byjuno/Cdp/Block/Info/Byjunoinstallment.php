56dd717cb14a67bf948677b8990217bb538f85c8
Igor <igor.sutugin@gmail.com>
2019-01-13 20:25:04 +0200

    frontend

--- code/local/Byjuno/Cdp/Block/Info/Byjunoinstallment.php
+++ code/local/Byjuno/Cdp/Block/Info/Byjunoinstallment.php
@@ -10,18 +10,21 @@ class Byjuno_Cdp_Block_Info_Byjunoinstallment extends Mage_Payment_Block_Info
 
     public function getInstructions()
     {
-        $methodsAllowed["installment_3_enable"] = 0;
-        $methodsAllowed["installment_10_enable"] = 1;
-        $methodsAllowed["installment_12_enable"] = 2;
-        $methodsAllowed["installment_24_enable"] = 3;
-        $methodsAllowed["installment_4x12_enable"] = 4;
-        $methodsAllowed["installment_4x10_enable"] = 5;
-
         $info = $this->getInfo()->getAdditionalInformation("is_b2b");
         if ($info == "true") {
-            $pl = explode(",", Mage::getStoreConfig('payment/cdp/byjuno_installment_paymentsb2b', Mage::app()->getStore()));
+            $methodsAllowed["installment_3_enable"] = Array("byjuno_installment_payments_3b2b", "byjuno_installment_payments_3_urlb2b");
+            $methodsAllowed["installment_10_enable"] = Array("byjuno_installment_payments_10b2b", "byjuno_installment_payments_10_urlb2b");
+            $methodsAllowed["installment_12_enable"] = Array("byjuno_installment_payments_12b2b", "byjuno_installment_payments_12_urlb2b");
+            $methodsAllowed["installment_24_enable"] = Array("byjuno_installment_payments_24b2b", "byjuno_installment_payments_24_urlb2b");
+            $methodsAllowed["installment_4x12_enable"] = Array("byjuno_installment_payments_4x12b2b", "byjuno_installment_payments_4x12_urlb2b");
+            $methodsAllowed["installment_4x10_enable"] = Array("byjuno_installment_payments_4x10b2b", "byjuno_installment_payments_4x10_urlb2b");
         } else {
-            $pl = explode(",", Mage::getStoreConfig('payment/cdp/byjuno_installment_payments', Mage::app()->getStore()));
+            $methodsAllowed["installment_3_enable"] = Array("byjuno_installment_payments_3", "byjuno_installment_payments_3_url");
+            $methodsAllowed["installment_10_enable"] = Array("byjuno_installment_payments_10", "byjuno_installment_payments_10_url");
+            $methodsAllowed["installment_12_enable"] = Array("byjuno_installment_payments_12", "byjuno_installment_payments_12_url");
+            $methodsAllowed["installment_24_enable"] = Array("byjuno_installment_payments_24", "byjuno_installment_payments_24_url");
+            $methodsAllowed["installment_4x12_enable"] = Array("byjuno_installment_payments_4x12", "byjuno_installment_payments_4x12_url");
+            $methodsAllowed["installment_4x10_enable"] = Array("byjuno_installment_payments_4x10", "byjuno_installment_payments_4x10_url");
         }
 
         $plId = $methodsAllowed[$this->getInfo()->getAdditionalInformation("payment_plan")];
@@ -48,20 +51,21 @@ class Byjuno_Cdp_Block_Info_Byjunoinstallment extends Mage_Payment_Block_Info
             $htmlAdd .= '<br>'.$this->__("Date of birth").": ".$this->getInfo()->getAdditionalInformation("dob_custom");
         }
 
-        $i = 0;
-        $stringValues = Array();
-        foreach($pl as $val) {
-            if (!strstr($val, "_enable")) {
-                $stringValues[$i] = $val;
-                $i++;
-            }
-        }
         $out = '(B2C)';
         if ($info == 'true') {
             $out = '(B2B)';
         } else if ($info == "") {
             $out = '(-)';
         }
-        return $stringValues[$plId] . ' '.$out.' - (<a href="'.$this->escapeHtml($stringValues[$plId + 6]).'" target="_blank">'.Mage::getStoreConfig('payment/cdp/byjuno_installment_toc_string', Mage::app()->getStore()).'</a>)'.$htmlAdd;
+
+        return Mage::getStoreConfig('payment/cdp/'.$plId[0], Mage::app()->getStore()) .
+        ' '.$out.' - (<a href="'.
+        $this->escapeHtml(Mage::getStoreConfig('payment/cdp/'.$plId[1], Mage::app()->getStore())).
+        '" target="_blank">'.
+        Mage::getStoreConfig('payment/cdp/byjuno_invoice_toc_string', Mage::app()->getStore())
+        .'</a>)'
+        .$htmlAdd;
+
+        //return $stringValues[$plId] . ' '.$out.' - (<a href="'.$this->escapeHtml($stringValues[$plId + 6]).'" target="_blank">'.Mage::getStoreConfig('payment/cdp/byjuno_installment_toc_string', Mage::app()->getStore()).'</a>)'.$htmlAdd;
     }
 }
